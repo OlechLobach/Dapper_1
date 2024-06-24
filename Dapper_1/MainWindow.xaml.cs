22e30d67b6d9cf4d63081c579a13f38f3363a669
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-06-25 00:27:50 +0300

    initiall commit

--- Dapper_1/MainWindow.xaml.cs
+++ Dapper_1/MainWindow.xaml.cs
@@ -1,182 +1,24 @@
-﻿using System;
-using Microsoft.Data.SqlClient;
+﻿using System.Text;
 using System.Windows;
-using Dapper;
-using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace WpfApp
+namespace Dapper_1
 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
-        string connectionString = "Server=DESKTOP-GNKQNCJ\\SQLEXPRESS;Database=MailingList;Integrated Security=True;TrustServerCertificate=True;";
-
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void ConnectButton_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                using (var connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    OutputTextBox.Text = "Підключення успішне!\n";
-
-                    DisplayAllCustomers(connection);
-                    DisplayAllEmails(connection);
-                    DisplayAllSections(connection);
-                    DisplayAllPromotions(connection);
-                    DisplayAllCities(connection);
-                    DisplayAllCountries(connection);
-                }
-            }
-            catch (Exception ex)
-            {
-                OutputTextBox.Text = $"Помилка підключення: {ex.Message}";
-            }
-        }
-
-        private void DisplayAllCustomers(SqlConnection connection)
-        {
-            var customers = connection.Query<Customer>("SELECT * FROM Customers");
-            OutputTextBox.Text += "Усі покупці:\n";
-            foreach (var customer in customers)
-            {
-                OutputTextBox.Text += $"{customer.CustomerID}: {customer.FullName}, {customer.Email}\n";
-            }
-        }
-
-        private void DisplayAllEmails(SqlConnection connection)
-        {
-            var emails = connection.Query<string>("SELECT Email FROM Customers");
-            OutputTextBox.Text += "Email усіх покупців:\n";
-            foreach (var email in emails)
-            {
-                OutputTextBox.Text += email + "\n";
-            }
-        }
-
-        private void DisplayAllSections(SqlConnection connection)
-        {
-            var sections = connection.Query<Section>("SELECT * FROM Sections");
-            OutputTextBox.Text += "Усі розділи:\n";
-            foreach (var section in sections)
-            {
-                OutputTextBox.Text += $"{section.SectionID}: {section.SectionName}\n";
-            }
-        }
-
-        private void DisplayAllPromotions(SqlConnection connection)
-        {
-            var promotions = connection.Query<Promotion>("SELECT * FROM Promotions");
-            OutputTextBox.Text += "Усі акційні товари:\n";
-            foreach (var promotion in promotions)
-            {
-                OutputTextBox.Text += $"{promotion.PromotionID}: {promotion.SectionID}, {promotion.Country}, {promotion.StartDate}, {promotion.EndDate}\n";
-            }
-        }
-
-        private void DisplayAllCities(SqlConnection connection)
-        {
-            var cities = connection.Query<string>("SELECT DISTINCT City FROM Customers");
-            OutputTextBox.Text += "Усі міста:\n";
-            foreach (var city in cities)
-            {
-                OutputTextBox.Text += city + "\n";
-            }
-        }
-
-        private void DisplayAllCountries(SqlConnection connection)
-        {
-            var countries = connection.Query<string>("SELECT DISTINCT Country FROM Customers");
-            OutputTextBox.Text += "Усі країни:\n";
-            foreach (var country in countries)
-            {
-                OutputTextBox.Text += country + "\n";
-            }
-        }
-
-        private void CityButton_Click(object sender, RoutedEventArgs e)
-        {
-            string city = CityTextBox.Text;
-            try
-            {
-                using (var connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    DisplayCustomersByCity(connection, city);
-                }
-            }
-            catch (Exception ex)
-            {
-                OutputTextBox.Text = $"Помилка підключення: {ex.Message}";
-            }
-        }
-
-        private void DisplayCustomersByCity(SqlConnection connection, string city)
-        {
-            var customers = connection.Query<Customer>("SELECT * FROM Customers WHERE City = @City", new { City = city });
-            OutputTextBox.Text += $"Покупці з міста {city}:\n";
-            foreach (var customer in customers)
-            {
-                OutputTextBox.Text += $"{customer.CustomerID}: {customer.FullName}, {customer.Email}\n";
-            }
-        }
-
-        private void CountryButton_Click(object sender, RoutedEventArgs e)
-        {
-            string country = CountryTextBox.Text;
-            try
-            {
-                using (var connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    DisplayCustomersByCountry(connection, country);
-                }
-            }
-            catch (Exception ex)
-            {
-                OutputTextBox.Text = $"Помилка підключення: {ex.Message}";
-            }
-        }
-
-        private void DisplayCustomersByCountry(SqlConnection connection, string country)
-        {
-            var customers = connection.Query<Customer>("SELECT * FROM Customers WHERE Country = @Country", new { Country = country });
-            OutputTextBox.Text += $"Покупці з країни {country}:\n";
-            foreach (var customer in customers)
-            {
-                OutputTextBox.Text += $"{customer.CustomerID}: {customer.FullName}, {customer.Email}\n";
-            }
-        }
-
-        private void CountryPromotionsButton_Click(object sender, RoutedEventArgs e)
-        {
-            string country = CountryTextBox.Text;
-            try
-            {
-                using (var connection = new SqlConnection(connectionString))
-                {
-                    connection.Open();
-                    DisplayPromotionsByCountry(connection, country);
-                }
-            }
-            catch (Exception ex)
-            {
-                OutputTextBox.Text = $"Помилка підключення: {ex.Message}";
-            }
-        }
-
-        private void DisplayPromotionsByCountry(SqlConnection connection, string country)
-        {
-            var promotions = connection.Query<Promotion>("SELECT * FROM Promotions WHERE Country = @Country", new { Country = country });
-            OutputTextBox.Text += $"Акції для країни {country}:\n";
-            foreach (var promotion in promotions)
-            {
-                OutputTextBox.Text += $"{promotion.PromotionID}: {promotion.SectionID}, {promotion.Country}, {promotion.StartDate}, {promotion.EndDate}\n";
-            }
         }
     }
 }
